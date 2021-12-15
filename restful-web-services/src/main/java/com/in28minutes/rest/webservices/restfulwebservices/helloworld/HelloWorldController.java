b650e7719a289e4cf79c92d67820d5fd206604c5
Haji Seferian <hajiseferian@gmail.com>
2021-12-15 15:59:05 -0600

    first pushhh

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,43 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import java.util.Locale;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - hello-world
+	//method - "hello world"
+	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path ="/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	//hello-world-bean
+	@GetMapping(path ="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean ("Hello World!");
+	}
+
+	//hello-world-bean
+	@GetMapping(path ="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean (String.format("Hello World!, %s", name));
+	}
+	
+	@GetMapping(path ="/hello-world-international")
+	public String helloWorldInternational(@RequestHeader(name="Accept-Language", required = false) Locale local) {
+		return "Hello World!";
+	}
+
+
+}
