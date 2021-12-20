1ad55dfeea3fba164605cd238d731ec6f365bbd4
Haji Seferian <hajiseferian@gmail.com>
2021-12-20 12:55:52 -0600

    Connecting to H2 Database and adding custom banner.

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- DROP table Contacts if exists;
+-- CREATE TABLE USER (
+--     id BIGINT IDENTITY NOT NULL,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+insert into user values(1,sysdate(),'ab');
+insert into user values(2,sysdate(),'cd');
+insert into user values(3,sysdate(),'ef');
